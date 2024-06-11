2ed7d8acb3ca94bba8453224e45be002e7226527
i.gorbunov <gorbunovi@rambler.ru>
2024-06-11 19:12:42 +0400

    Обновление flutter

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,9 +5,9 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-import 'package:cookbook/feature/routes/app_pages.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:cookbook/feature/routes/app_pages.dart';
 
 
 void main() {
